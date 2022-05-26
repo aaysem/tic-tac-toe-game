0d7f83e7734c534444af4046782a77a9d557863b
Ayşem <aaysemguler@gmail.com>
2022-05-26 21:48:53 +0300

    Initial commit

---
+++ tictactoegame/Main.java
@@ -0,0 +1,15 @@
+public class Main extends Thread{
+
+	public void run() {
+		TicTacToeGame ttt = new TicTacToeGame();
+	}
+
+
+
+	public static void main(String[] args) {
+
+		 new TicTacToeGame();
+
+
+	}
+}
